287a4b40006718d088a113f96041808928f21d61
CorentinDumery <43251496+CorentinDumery@users.noreply.github.com>
2020-01-20 13:12:58 +0800

    Improve readability

--- potato.py
+++ potato.py
@@ -7,6 +7,17 @@ from math import sin,cos,sqrt
 from random import randrange,random
 import numpy as np
 
+## Change the number of perturbations here
+numberPerturbations = 100
+
+## Change the amplitude/frequency
+## For better results, try to give low amplitude to high frequencies signals
+def amplitude(i):
+    return (100-i)*randrange(100)/10000
+
+def frequency(i):
+    return i*randrange(50)/(10000)
+
 class Vertex:
     def __init__(self, v3):
         self.x = v3[0]
@@ -24,7 +35,6 @@ class Vertex:
         
     def __str__(self): 
         return "V("+str(self.x)[0:5]+","+str(self.y)[0:5]+","+str(self.z)[0:5]+")"
-
     def __add__(self, v2):
         return Vertex([self.x+v2.x,self.y+v2.y,self.z+v2.z])
     def __sub__(self, v2):
@@ -35,22 +45,6 @@ class Vertex:
         return Vertex([self.x*f,self.y*f,self.z*f])
     def __mul__(self, f):
         return Vertex([self.x*f,self.y*f,self.z*f])
-    def __eq__(self,v2):
-        return abs(self.x-v2.x)<epsilon and abs(self.y-v2.y)<epsilon and abs(self.z-v2.z)<epsilon
-    def __ge__(self,v2):
-        if self.x > v2.x+epsilon:
-            return True
-        if self.x < v2.x-epsilon:
-            return False
-        if self.y > v2.y+epsilon:
-            return True
-        if self.y < v2.y-epsilon:
-            return False
-        if self.z > v2.z+epsilon:
-            return True
-        if self.z < v2.z-epsilon:
-            return False
-        return True
     def __le__(self,v2):
         return (self==v2) or not(self >= v2)
     def __lt__(self,v2):
@@ -65,7 +59,7 @@ class Vertex:
         return sqrt(self.x**2 +self.y**2 +self.z**2)
     
     
-def genSph(n=81,m=64): #outputs a disk
+def genSph(n=81,m=64):
     res = []
     scale = 10
     p0 = [0,0,1*scale]
@@ -83,38 +77,24 @@ def genSph(n=81,m=64): #outputs a disk
             res.append([layers[-1][j],layers[-1][(j+1)%m],l[j]])
             res.append([layers[-1][(j+1)%m],l[(j+1)%m],l[j]])
         layers.append(l)
-        
     
-    waves = 10
+    res0 = res #output res0 to see intermediate steps
     
-    res0 = res
-    
-    for i in range(100):
-        
-        # début : petite fréquence grande amplitude
-        # fin : grande f petit A
-        
+    for i in range(numberPerturbations):
         direction = Vertex([randrange(100)-50,randrange(100)-50,randrange(100)-50])
         direction = direction/direction.norm()
-        #print(direction)
-        #direction=Vertex([0.3,0.8,0])
         r = (random()-0.5)*2
-        off = randrange(100)
-        #direction = Vertex([1,0,0])
-        ampl = (60-i)*scale*randrange(100)/100000
-        freq = i*0.01*randrange(50)/(5*scale*2)
-        #print("dir amp freq r",direction,ampl,freq,r)
+        off = randrange(100) #offset for sinus function
+        ampl = amplitude(i) #ampl = (100-i)*scale*randrange(100)/100000
+        freq = frequency(i) #freq = i*0.01*randrange(50)/(5*scale*2)
         for j in range(len(res)):
             for k in range(3):
                 v = Vertex(res[j][k])
                 proj = v.dot(direction)
-                #res[j][k]=[v.x + r*sin(off+2*3.14*freq*v.x)*direction.x,v.y + r*sin(off+2*3.14*freq*v.y)*direction.y,v.z + r*sin(off+2*3.14*freq*v.z)*direction.z]
                 res[j][k]=[v.x + r*sin(off+2*3.14*freq*proj)*ampl*direction.x,v.y + r*sin(off+2*3.14*freq*proj)*ampl*direction.y,v.z + r*sin(off+2*3.14*freq*proj)*ampl*direction.z]
         if i%10 ==0:
             res0 = res0 + offsetStl(res,i*10,0,0)
-    
     sendOut(res)
-    #return Mesh(res)
     
 def sendOut(outData):
     data = np.zeros(len(outData), dtype=mesh.Mesh.dtype)
